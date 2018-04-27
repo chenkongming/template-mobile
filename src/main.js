da7aab8b5fa431f859499342a052640ebf53cfd8
chenkongming <337142363@qq.com>
2018-04-27 23:48:47 +0800

    引入FastClick

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
+import FastClick from 'fastclick';
 Vue.config.productionTip = false;
-
+FastClick.attach(document.body);
 /* eslint-disable no-new */
 new Vue({
 	el: '#app',
